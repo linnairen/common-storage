0198cb0a5addd6e62cf5a4469f4e30a893741b21
linnairen <linnairen@aiqinhaigou.com>
2021-04-09 14:43:51 +0800

    export change

--- dist/index.ts
+++ dist/index.ts
@@ -1,6 +1,11 @@
 import { CommonWebSql } from './websql'
 import { CommonStore } from './localstore'
 
+export {
+  CommonWebSql,
+  CommonStore
+}
+
 export default {
   CommonWebSql,
   CommonStore
